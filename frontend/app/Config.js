6ebecb268a63a7e28fb329c03493376c215d7ff5
Sean Morris <noreply@seanmorr.is>
2018-12-24 21:32:11 -0500

    Loose ends.

--- frontend/app/Config.js
+++ frontend/app/Config.js
@@ -5,3 +5,4 @@ export class Config {
 // Config.backend = '//api.isotope.local.seanmorr.is';
 // Config.backend = '//isotope-backend:9997';
 Config.backend = '//localhost';
+Config.title   = 'isotope';
